0dd5a7ae7f07fa50ce7d140efe48feb92c27dd3e
iamheesoo <beforeheesoo@gmail.com>
2023-10-24 18:54:57 +0900

    [Add] CoordinatorLayout 샘플 코드 추가

--- app/src/main/java/com/example/androidpractice/coordinate/CoordinateMainFragment.kt
+++ app/src/main/java/com/example/androidpractice/coordinate/CoordinateMainFragment.kt
@@ -1,10 +1,12 @@
 package com.example.androidpractice.coordinate
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidpractice.databinding.FragmentCoordinateMainBinding
 
 class CoordinateMainFragment : Fragment() {
@@ -12,6 +14,8 @@ class CoordinateMainFragment : Fragment() {
     private val binding: FragmentCoordinateMainBinding by lazy {
         FragmentCoordinateMainBinding.inflate(LayoutInflater.from(context))
     }
+    private val adapter = TitleAdapter()
+    private val list = List<String>(100) { it.toString() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +23,16 @@ class CoordinateMainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            Log.i("!!!", "onViewCreated")
+            rvMain.adapter = adapter
+            rvMain.layoutManager = LinearLayoutManager(root.context, LinearLayoutManager.VERTICAL, false)
+            adapter.setData(list)
+        }
     }
 }
--- app/src/main/java/com/example/androidpractice/coordinate/TitleAdapter.kt
+++ app/src/main/java/com/example/androidpractice/coordinate/TitleAdapter.kt
@@ -1,23 +1,37 @@
-//package com.example.androidpractice.coordinate
-//
-//import android.view.ViewGroup
-//import androidx.recyclerview.widget.RecyclerView
-//
-//class TitleAdapter : RecyclerView.Adapter<TitleViewHolder>() {
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TitleViewHolder {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun onBindViewHolder(holder: TitleViewHolder, position: Int) {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun getItemCount(): Int {
-//        TODO("Not yet implemented")
-//    }
-//
-//}
-//
-//class TitleViewHolder : RecyclerView.ViewHolder() {
-//
-//}
+package com.example.androidpractice.coordinate
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidpractice.databinding.ItemTitleBinding
+
+class TitleAdapter : RecyclerView.Adapter<TitleAdapter.TitleViewHolder>() {
+
+    private var list = emptyList<String>()
+
+    class TitleViewHolder(private val binding: ItemTitleBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: String) {
+            Log.i("!!!", "$data")
+            binding.tvTitle.text = data
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TitleViewHolder {
+        val binding = ItemTitleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TitleViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TitleViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    fun setData(list: List<String>){
+        this.list = list
+        notifyDataSetChanged()
+    }
+}
